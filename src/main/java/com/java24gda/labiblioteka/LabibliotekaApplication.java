c9c3913c97a84399407effa592b668913fb83379
Dariusz.Żulpo <dd2000@wp.pl>
2019-10-06 09:46:58 +0200

    niedziela labiblioteka

---
+++ src/main/java/com/java24gda/labiblioteka/LabibliotekaApplication.java
@@ -0,0 +1,13 @@
+package com.java24gda.labiblioteka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabibliotekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabibliotekaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/java24gda/labiblioteka/LabibliotekaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.java24gda.labiblioteka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LabibliotekaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
